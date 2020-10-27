41b483c164dc66814143f99892419c386a9229d9
Edmundo Biglia <edmundobiglia@gmail.com>
2020-10-26 22:51:13 -0300

    Updates

---
+++ precache-manifest.7005ea02bca2ca2d60716f29d68938b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe567482ff029a3fe6fbd75457ff420",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "6ba85d1e2608f862e8c3",
+    "url": "/tic-tac-toe/static/js/2.a457200a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.a457200a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06e8ce0ebacadf96986",
+    "url": "/tic-tac-toe/static/js/main.34c178a4.chunk.js"
+  },
+  {
+    "revision": "462713278a3dc921614c",
+    "url": "/tic-tac-toe/static/js/runtime-main.dade6f86.js"
+  }
+]);
